d000863d67264f52074550fad5c50359389862f3
NegaTiv1298 <levkristi@i.ua>
2022-02-20 20:44:34 +0200

    Add routes for Rest API

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,30 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/**
+ * Adding routes for ari requests.
+ * Request example : http://testcrm.loc/api/v1/get/companies/paginate=/10
+ */
+Route::prefix('/get/companies')->group(function () {
+    Route::get('/', [\App\Http\Controllers\CompanyController::class, 'getCompaniesForApi']);
+    Route::prefix('/paginate=')->group(function () {
+        Route::get('/{paginate}', [\App\Http\Controllers\CompanyController::class, 'getCompaniesForApi']);
+    });
+});
+
+/**
+ * Request example : http://testcrm.loc/api/v1/get/clients/company_id/554/paginate=/1
+ */
+Route::prefix('/get/clients/company_id/{id}')->group(function () {
+    Route::get('/', [\App\Http\Controllers\CustomerController::class, 'getCustomersForApi']);
+    Route::prefix('/paginate=')->group(function () {
+        Route::get('/{paginate}', [\App\Http\Controllers\CustomerController::class, 'getCustomersForApi']);
+    });
+});
+
+/**
+ * Request example : http://testcrm.loc/api/v1/get/clients_companies/554
+ */
+Route::get('/get/clients_companies/{id}', [\App\Http\Controllers\CompanyController::class, 'getCompanyByCustomerIdForApi']);
+
